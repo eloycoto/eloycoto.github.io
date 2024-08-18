c5885cea7bfab8d0fd33c9f76a18706a1e1daaf0
Eloy Coto <eloy.coto@acalustra.com>
2024-08-18 21:07:17 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+ { 
+  description = "blog";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+
+          python-packages = ps: with ps; [
+            pelican
+          ];
+
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            buildInputs = [
+              (pkgs.python3.withPackages python-packages)
+              pkgs.gnumake
+            ];
+          };
+        }
+      );
+}
